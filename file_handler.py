c6ece1b416d3b22ff037c3996cdcbe0286cb7b62
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-04-04 23:02:20 +0100

    Fixing image reading/pathing issue, adding labels/masks

--- file_handler.py
+++ file_handler.py
@@ -12,22 +12,26 @@ def read_img(filename):
 
 def save_img(image, filename):
     print("Saving file: " + filename)
+    print(image)
     cv2.imwrite(filename, image)
 
 def convert_image_to_greyscale(path):
     for file in get_files(path):
         print("Reading file: " + file)
-        img = read_img(file)
+        img = read_img(path + file)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         save_img(gray, file)
 
-def grab_label_plates(label_img_path):
+def grab_plates(label_img_path):
     for file in get_files(label_img_path):
-        yield read_img(file), file
+        yield read_img(label_img_path + file), file
 
-def save_label_plates(img, file, save_path):
-    save_img(img, save_path + file)
+def save_label_plates(img_path, save_path):
+    for (img, file) in grab_plates(img_path):
+        print("Saving img file: " + file + " to: " + save_path)
+        print("Savepath: " + save_path)
+        save_img(img, save_path + file)
 
 def grab_image_plates(label_img_path):
-    for (image, file) in grab_label_plates(label_img_path):
+    for (image, file) in grab_plates(label_img_path):
         print("Grabbed file: " + file)
